beed7e51ed42dc5999c5f676aea94d258eb23f12
Alison Doyle <s00233102@atu.ie>
2024-11-11 18:56:20 +0000

    :construction: Working on admin ui

--- src/app/routes/admin-dashboard/admin-dashboard.component.ts
+++ src/app/routes/admin-dashboard/admin-dashboard.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { FullCalendarModule} from '@fullcalendar/angular';
+import { CalendarOptions } from '@fullcalendar/core'; // useful for typechecking
+import dayGridPlugin from '@fullcalendar/daygrid';
 
 @Component({
   selector: 'app-admin-dashboard',
   standalone: true,
-  imports: [],
+  imports: [
+    FullCalendarModule
+  ],
   templateUrl: './admin-dashboard.component.html',
   styleUrl: './admin-dashboard.component.scss'
 })
-export class AdminDashboardComponent {
 
+export class AdminDashboardComponent {
+  // Properties
+  protected calendarOptions: CalendarOptions = {
+    initialView: 'dayGridMonth',
+    plugins: [
+      dayGridPlugin
+    ]
+  };
 }
