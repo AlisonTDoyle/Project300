4a5e3e6a20bccae60b470dfa1efc1c7b092d09b6
GreenThunder <123633616+SamuelAyres76@users.noreply.github.com>
2025-02-09 16:41:21 +0000

    Login Works!

--- src/app/auth/auth.config.ts
+++ src/app/auth/auth.config.ts
@@ -3,7 +3,7 @@ import { PassedInitialConfig } from 'angular-auth-oidc-client';
 export const authConfig: PassedInitialConfig = {
   config: {
               authority: 'https://cognito-idp.eu-west-1.amazonaws.com/eu-west-1_henv86fnL',
-              redirectUrl: window.location.origin,
+              redirectUrl: 'https://d84l1y8p4kdic.cloudfront.net',
               postLogoutRedirectUri: window.location.origin,
               clientId: '4c8prqqjs00mht0u4flids2mm8',
               scope: 'email openid phone', // 'openid profile offline_access ' + your scopes
