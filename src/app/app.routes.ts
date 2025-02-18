f465aee926762122422658337b832096fbe20bbe
AlisonTDoyle <s00233102@atu.ie>
2025-02-18 21:44:58 +0000

    :sparkles: Got room child route working

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,26 @@
 import { Routes } from '@angular/router';
 import { AdminDashboardComponent } from './routes/admin-dashboard/admin-dashboard.component';
 import { ScheduleComponent } from './routes/schedule/schedule.component';
+import { RoomManagerComponent } from './components/admin-dashboard/room-manager/room-manager.component';
 
 export const routes: Routes = [
-    { path: '', component: ScheduleComponent },
-    { path: 'schedule', redirectTo: '/'},
-    { path: 'admin', component: AdminDashboardComponent },
+    {
+        path: '',
+        component: ScheduleComponent
+    },
+    {
+        path: 'schedule',
+        redirectTo: '/'
+    },
+    {
+        path: 'admin',
+        title: 'Admin Dashboard',
+        component: AdminDashboardComponent,
+        children: [
+            {
+                path: 'room-manager',
+                component: RoomManagerComponent
+            }
+        ]
+    }
 ];
--- src/app/components/admin-dashboard/admin-header/admin-header.component.ts
+++ src/app/components/admin-dashboard/admin-header/admin-header.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-admin-header',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLinkActive,
+    RouterLink
+  ],
   templateUrl: './admin-header.component.html',
   styleUrl: './admin-header.component.scss'
 })
