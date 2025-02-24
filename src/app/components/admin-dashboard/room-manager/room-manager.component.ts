a5391d1e5b1c467532c3d486aa1cc4cd9dbddd6f
AlisonTDoyle <s00233102@atu.ie>
2025-02-24 19:15:32 +0000

    :construction: Limited display of events in timetable

--- src/app/components/admin-dashboard/room-manager/room-manager.component.ts
+++ src/app/components/admin-dashboard/room-manager/room-manager.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { FullCalendarModule } from '@fullcalendar/angular';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FullCalendarComponent, FullCalendarModule } from '@fullcalendar/angular';
 import { CalendarOptions, DateSelectArg, EventApi, EventClickArg, EventInput } from '@fullcalendar/core';
 import interactionPlugin, { EventDragStopArg } from '@fullcalendar/interaction'
 import * as bootstrap from "bootstrap";
@@ -9,6 +9,7 @@ import { CommonModule } from '@angular/common';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { DatabaseApiService } from '../../../services/database-api/database-api.service';
 import { Room } from '../../../interfaces/room';
+import { TimetableApiService } from '../../../services/timetable-api/timetable-api.service';
 
 @Component({
   selector: 'app-room-manager',
@@ -40,24 +41,7 @@ export class RoomManagerComponent implements OnInit {
     slotMinTime: "08:00:00",
     slotMaxTime: "22:00:00",
     eventColor: '#378006',
-    events:  [
-      {
-        title: 'Maths',
-        startTime: "9:00",
-        endTime: "10:00",
-        start: '2025-02-20T09:00:00',
-        end: '2025-02-20T10:00:00',
-        daysOfWeek: [1] // Monday
-      },
-      {
-        title: 'Intro To Programming',
-        startTime: "11:00",
-        endTime: "13:00",
-        start: '2025-02-20T11:00:00',
-        end: '2025-02-20T13:00:00',
-        daysOfWeek: [1] // Monday
-      }
-    ],
+    events:  [],
     eventDidMount: (info) => {
       return new bootstrap.Popover(info.el, {
         title: info.event.title,
@@ -66,23 +50,58 @@ export class RoomManagerComponent implements OnInit {
         content: `<p>${info.event.extendedProps['roomNumber']} (${info.event.extendedProps['room']})</p>`,
         html: true
       })
-    },
+    }
   };
-
   protected roomAction:string = "Create";
   protected rooms:Room[] = [];
-  private roomCursor:Object = {};
+
+  private _roomCursor:Object = {};
+  private _selectedRoom:Room|null = null;
+
+  @ViewChild('programPreview') calendarComponent: FullCalendarComponent | null = null;
 
   // Constructor
-  constructor(private _databaseApi:DatabaseApiService) { }
+  constructor(private _databaseApi:DatabaseApiService, private _timetableApi:TimetableApiService) { }
 
-  // Event listeners
+  // Event 
   ngOnInit(): void {
     this._databaseApi.ReadRoomsWithPagination(20, {}).subscribe((res) => {
       this.rooms = res.rooms;
-      this.roomCursor = res.cursor;
+      this._roomCursor = res.cursor;
     });
   }
 
   // Methods
+  protected FetchRoomTimetable(room:Room) {
+    // Capture the selected room
+    this._selectedRoom = room;
+
+    // Get the calendar API
+    let calendarApi = this.calendarComponent?.getApi();
+
+    // Fetch the timetable for the selected room
+    if (calendarApi != null) {
+      // Clear the current events
+      calendarApi.removeAllEvents();
+
+      this._timetableApi.ReadRoomTimetable(this._selectedRoom.RoomNo).subscribe((res) => {
+        console.log(res);
+
+        for (let i = 0; i < res.length; i++) {
+          let newEvent = {
+            title: "Test",
+            startTime: res[i]?.StartTime,
+            endTime: res[i]?.EndTime,
+            startRecur: "2024-11-11T11:00:00.000Z",
+            daysOfWeek: [2],
+            extendedProps: {
+              roomNumber: res[i]?.RoomNo
+            }
+          }
+
+          calendarApi?.addEvent(newEvent);
+        }
+      });
+    }
+  }
 }
