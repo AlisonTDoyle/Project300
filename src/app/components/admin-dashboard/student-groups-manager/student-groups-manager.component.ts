cb3c8643b092a06bdce9450ada364fb7d8d468a1
AlisonTDoyle <s00233102@atu.ie>
2025-02-25 22:07:14 +0000

    :hammer: Implement initial setup for Student Groups Manager with calendar integration

--- src/app/components/admin-dashboard/student-groups-manager/student-groups-manager.component.ts
+++ src/app/components/admin-dashboard/student-groups-manager/student-groups-manager.component.ts
@@ -1,15 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { EventManagementFormComponent } from '../event-management-form/event-management-form.component';
+import { FullCalendarModule } from '@fullcalendar/angular';
+import { CalendarOptions } from '@fullcalendar/core';
 
 @Component({
   selector: 'app-student-groups-manager',
   standalone: true,
   imports: [
-    EventManagementFormComponent
+    EventManagementFormComponent,
+    FullCalendarModule
   ],
   templateUrl: './student-groups-manager.component.html',
   styleUrl: './student-groups-manager.component.scss'
 })
-export class StudentGroupsManagerComponent {
+export class StudentGroupsManagerComponent implements OnInit {
+  // Properties
+  private _studentGroupsCursor: object = {};
+  private _selectedStudentGroup: object | null = null;
 
+  protected studentGroups: object[] = [];
+  protected calendarOptions: CalendarOptions = {};
+
+  // Constructor
+  constructor() {
+  }
+
+  // Event handlers
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+  // Methods
 }
