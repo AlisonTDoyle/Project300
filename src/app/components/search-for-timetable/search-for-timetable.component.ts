772c33ca55ba27e3b1be0a96b86bc01727423550
MizunashiEnigma <123388502+MizunashiEnigma@users.noreply.github.com>
2025-02-27 14:09:03 +0000

    created search for timetable componenets

---
+++ src/app/components/search-for-timetable/search-for-timetable.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-search-for-timetable',
+  imports: [],
+  templateUrl: './search-for-timetable.component.html',
+  styleUrl: './search-for-timetable.component.scss'
+})
+export class SearchForTimetableComponent {
+
+}
