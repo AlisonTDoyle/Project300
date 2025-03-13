245d57a81fef95ba0de4de547343eee28090f01a
MizunashiEnigma <123388502+MizunashiEnigma@users.noreply.github.com>
2025-03-13 18:22:05 +0000

    Cognito stuff. doesnt work. made to revert branch

--- src/app/components/shared/header/header.component.ts
+++ src/app/components/shared/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet,RouterLink,RouterLinkActive } from '@angular/router';
+
 @Component({
   selector: 'app-header',
   standalone: true,
