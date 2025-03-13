48886d55cfa38f11378f6e7b872d643b061587d7
MizunashiEnigma <123388502+MizunashiEnigma@users.noreply.github.com>
2025-03-13 18:04:05 +0000

    Removed the cognito things

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,9 +2,6 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
 import { routes } from './app.routes';
-import { authConfig } from './auth/auth.config';
-import { provideAuth } from 'angular-auth-oidc-client';
-
 
 export const appConfig: ApplicationConfig = {
   providers: [
