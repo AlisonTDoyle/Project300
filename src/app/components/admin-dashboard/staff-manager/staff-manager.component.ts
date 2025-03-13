348e338d7c962c62d4561d514e1a87c0152fc7f9
AlisonDoyle-SleaterSolutions <adoyle@sleatersolutions.com>
2025-03-13 15:29:32 +0000

    Implement staff manager layout with event management form and timetable display

--- src/app/components/admin-dashboard/staff-manager/staff-manager.component.ts
+++ src/app/components/admin-dashboard/staff-manager/staff-manager.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { EventManagementFormComponent } from '../event-management-form/event-management-form.component';
+import { FullCalendarComponent, FullCalendarModule } from '@fullcalendar/angular';
 
 @Component({
   selector: 'app-staff-manager',
   standalone: true,
-  imports: [],
+  imports: [
+    EventManagementFormComponent,
+    FullCalendarModule
+  ],
   templateUrl: './staff-manager.component.html',
   styleUrl: './staff-manager.component.scss'
 })
 export class StaffManagerComponent {
-
+  // properties
+  protected selectedStaffMember:object|null = null;
 }
